5993f04dfa74de7f0d216f32facfd1e4c23bbba6
Kahu Anderson <mrlbrick@gmail.com>
2023-08-14 10:44:03 +1200

    Using FlexiHPC Object storage for Terraform state file

--- provider.tf
+++ provider.tf
@@ -7,6 +7,18 @@ required_version = ">= 0.14.0"
       version = "~> 1.51.1"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "state/terraform.tfstate"
+    endpoint   = "https://object.akl-1.cloud.nesi.org.nz/"
+    sts_endpoint = "https://object.akl-1.cloud.nesi.org.nz/"
+    access_key = "<EC2 User Access Token>"
+    secret_key = "<EC2 User Secret Token>"
+    #region = "us-east-1"
+    force_path_style = "true"
+    skip_credentials_validation = "true"
+  }
 }
 
 # Configure the OpenStack Provider
